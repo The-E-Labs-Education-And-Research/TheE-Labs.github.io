0793d7ccace7ab8ef1aa86c204007f3fa9135f1e
Saurabh Sakhare <saurabhs0294@gmail.com>
2023-12-17 17:42:14 +0530

    fix card and alignment

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,3 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('C Programming Card loaded successfully!');
 });
-
-let slides = document.querySelectorAll('.slide');
-let currentIndex = 0;
-
-function showSlide(index) {
-    slides[currentIndex].classList.remove('active');
-    slides[index].classList.add('active');
-    currentIndex = index;
-}
-
-function nextSlide() {
-    showSlide((currentIndex + 1) % slides.length);
-}
-
-// Start slideshow
-setInterval(nextSlide, 5000); // every 5 seconds
